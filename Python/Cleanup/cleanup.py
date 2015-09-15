e383e417f0d298cdd09cc595e58e9b79c8a582b4
Matthew Berryman <mberryma@uow.edu.au>
2015-09-15 13:10:34 +1000

    Cleaned up cleanup.py code

--- Python/Cleanup/cleanup.py
+++ Python/Cleanup/cleanup.py
@@ -1,25 +1,11 @@
-import csv
-with open('output_4.txt', 'r') as file:
-	reader = csv.reader(file, delimiter=" ") # delimiter takes the one character string \t representing tab to specify the field separator
-	d = list(reader) # creates a list in variable d containing the elements in reader
-output = open('output_4_clean.txt', 'w')
-	
-n=0
-replace=0
+#!/usr/bin/env python3
+if len(sys.argv) < 2:
+	print('Usage: python3 search_engine.py initialisms_file.txt')
+	print('  where the initialisms file contains initialisms of interest (all of the same length)')
+	exit(1)
+reader = csv.reader(sys.argv[1], delimiter=" ") # delimiter takes the one character string \t representing tab to specify the field separator
 
-for element in d:
-	line = d[n]
-	m=0
-	for word in line:
-		replace=0
-		for letter in word:
-			if letter=='_':
-				head, middle, tail = word.partition('_')
-				replace=1	
-		if replace==1:
-			output.write(str(head)+' ')
-		else:
-			output.write(str(word)+' ')
-	output.write('\n')
-	n=n+1
-output.close()
+	# Handle it line by line:
+for row in reader:
+	for word in row:
+		word = word[0:word.find('_')]
