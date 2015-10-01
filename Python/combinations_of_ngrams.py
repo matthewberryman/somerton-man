98ee7c177f64e922054c06120ab21a5912b9e601
Matthew Berryman <mberryma@uow.edu.au>
2015-10-01 13:04:46 +1000

    Minor fixes
    Import itertools.
    Refer to internally generated list of filenames.
    Add blank line between combinations for greater clarity.

--- Python/combinations_of_ngrams.py
+++ Python/combinations_of_ngrams.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, itertools
 
 
 code_segments = [ 'WRGOA', 'BABDM', 'LIAOI', 'WTBIM', 'PANET', 'PMLIA', 'BOAIA', 'QCITT', 'MTSAM', 'STGAB' ]
@@ -13,7 +13,7 @@ for code in code_segments:
 
 top_ngrams = list()
 
-for filename in sys.argv[1:]:
+for filename in filenames:
     tuples = list()
     for line in open(filename,'r'):
         row = line.split()
@@ -21,4 +21,5 @@ for filename in sys.argv[1:]:
     top_ngrams.append([x[1] for x in sorted(tuples)[0:2]])
 
 for x in [' '.join(s) for s in itertools.product(*top_ngrams)]:
-        print(x)
+    print(x)
+    print()
