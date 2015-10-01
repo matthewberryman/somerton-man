072c92f58d18b54e95873d4ff6af01423bb0a7a7
Matthew Berryman <mberryma@uow.edu.au>
2015-10-01 12:31:26 +1000

    combinations_of_grams.py
    Generate combinations of top 2 non-overlapping 5grams, based on the
    code and search outputs.

---
+++ Python/combinations_of_ngrams.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+code_segments = [ 'WRGOA', 'BABDM', 'LIAOI', 'WTBIM', 'PANET', 'PMLIA', 'BOAIA', 'QCITT', 'MTSAM', 'STGAB' ]
+#code_segments = [ 'WRGOA', 'BABDM', 'LIAOI', 'WTBIM', 'PANET', 'PMLIA', 'BOAIA', 'QCITT', 'MTSAM', 'STGAB' ]
+#code_segments = [ 'WRGOA', 'BABDM', 'LIAOI', 'WTBIM', 'PANET', 'PMLIA', 'BOAIA', 'QCITT', 'MTSAM', 'STGAB' ]
+
+filenames = list()
+for code in code_segments:
+    filenames.append(code+'_unique.txt')
+
+top_ngrams = list()
+
+for filename in sys.argv[1:]:
+    tuples = list()
+    for line in open(filename,'r'):
+        row = line.split()
+        tuples.append((row[0],' '.join(row[1:])))
+    top_ngrams.append([x[1] for x in sorted(tuples)[0:2]])
+
+for x in [' '.join(s) for s in itertools.product(*top_ngrams)]:
+        print(x)
